d92f2892d8dd9364b30f79b37c2339a6615fb0e7
LoaayElkherbawy <loaay.alkherbawy@gmail.com>
2024-08-04 17:23:21 +0300

    Updating and Fixing

--- lib/src/autocomplete_search.dart
+++ lib/src/autocomplete_search.dart
@@ -1,11 +1,11 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:google_maps_place_picker_mb/google_maps_place_picker.dart';
-import 'package:google_maps_place_picker_mb/providers/place_provider.dart';
-import 'package:google_maps_place_picker_mb/providers/search_provider.dart';
-import 'package:google_maps_place_picker_mb/src/components/prediction_tile.dart';
-import 'package:google_maps_place_picker_mb/src/controllers/autocomplete_search_controller.dart';
+import 'package:google_maps_place_picker_lks/google_maps_place_picker.dart';
+import 'package:google_maps_place_picker_lks/providers/place_provider.dart';
+import 'package:google_maps_place_picker_lks/providers/search_provider.dart';
+import 'package:google_maps_place_picker_lks/src/components/prediction_tile.dart';
+import 'package:google_maps_place_picker_lks/src/controllers/autocomplete_search_controller.dart';
 import 'package:flutter_google_maps_webservices/places.dart';
 import 'package:provider/provider.dart';
 
